1e465574cf13bd9f236362479030ffadf289a400
Sybille Sirky <105552227+sybillesirky@users.noreply.github.com>
2025-07-27 18:38:53 +0200

    black menu

--- src/cosmetics/menu/menucolors.cpp
+++ src/cosmetics/menu/menucolors.cpp
@@ -1,17 +1,7 @@
 #include "menucolors.hpp"
 #include "lib/stdlib.hpp"
 
-constexpr std::array<RGBA32, 11> CustomMenuColors = {{
-    {0x7f, 0xff, 0xd4},	// Aquamarineblue
-    {0x41, 0x69, 0xe1},	// Royalblue
-    {0x99, 0x00, 0x00},	// CrimsonRed
-    {0xFF, 0x00, 0xCB},	// Magenta
-    {0xFF, 0xC0, 0xCB},	// Pink
-    {0x8f, 0x00, 0xFF},	// Violet
-    {0x22, 0x8B, 0x22},	// ForestGreen
-    {0xFF, 0x70, 0x00},	// Orange
-    {0xFF, 0xD7, 0x00},	// gold
-    {0x5d, 0x43, 0x2c},	// WalnutBrown
+constexpr std::array<RGBA32, 1> CustomMenuColors = {{
     {0x00, 0x00, 0x00}	// black
 }};
 
@@ -25,9 +15,5 @@ ASMUsed void RandomizeCustomMenuColors(GraphicalObject *graphicalObject, MainMen
     // originally coded by joe a tron as gecko code, ported to C++
 	constexpr auto vanillaColorCount = 6;
 	constexpr auto customColorCount = CustomMenuColors.size();
-    const u32 random = lbl_RNG_Number(customColorCount + vanillaColorCount); // +6 to account for vanilla colors
-    if (random >= vanillaColorCount) {
-        object->colorPreset = 0x3; // set it to gray to tint it better
-        graphicalObject->tint = CustomMenuColors[random - vanillaColorCount];
-    }
+    graphicalObject->tint = CustomMenuColors[0];
 }
